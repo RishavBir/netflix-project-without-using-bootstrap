240e9fe79bab5ccd9706017c73c6e53c7bad8e64
Rishav Bir <rishavbir305@gmail.com>
2022-10-20 10:04:17 +0530

    done

---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+
+const SData =[
+
+{
+    sname:"Peaky Blinder",
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BZjYzZDgzMmYtYjY5Zi00YTk1LThhMDYtNjFlNzM4MTZhYzgyXkEyXkFqcGdeQXVyMTE5NDQ1MzQ3._V1_.jpg",
+    title:"Netflix Original Series",
+    links:"https://www.netflix.com/in/title/80002479",
+},
+{
+    sname:"Money Heist",
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BODI0ZTljYTMtODQ1NC00NmI0LTk1YWUtN2FlNDM1MDExMDlhXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_.jpg",
+    title:"Netflix Original Series",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Lucifer",
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    title:"Netflix Original Series",
+    links:"https://www.netflix.com/in/title/80057918",
+},
+{
+    sname:"The Walking Dead",
+    imgsrc:"https://m.media-amazon.com/images/I/81zqK4i3H0S.jpg",
+    title:"Netflix Original Series",
+    links:"https://www.netflix.com/in/title/70177057",
+},
+{
+    sname:"Kota Factory",
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BNWFlNTY5MzEtNDhhMy00YTRmLWJhYjMtM2JjODkyMGRkZmUzXkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_.jpg",
+    title:"Netflix Original Series",
+    links:"https://www.netflix.com/in/title/81249783"
+}
+]
+
+export default SData;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react"
+import ReactDOM from "react-dom"
+import Card from "./Card"
+import "./index.css"
+import SData from "./Sdata"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(
+  <>
+  <h1 className="heading_style">List Of Top 5 Netflix Series</h1>
+    <Card imgsrc={SData[0].imgsrc}
+      title={SData[0].title}
+      sname={SData[0].sname}
+      link={SData[0].links}/>
+
+<Card imgsrc={SData[1].imgsrc}
+      title={SData[1].title}
+      sname={SData[1].sname}
+      link={SData[1].links}/>
+
+<Card imgsrc={SData[2].imgsrc}
+      title={SData[2].title}
+      sname={SData[2].sname}
+      link={SData[2].links}/>
+
+<Card imgsrc={SData[3].imgsrc}
+      title={SData[3].title}
+      sname={SData[3].sname}
+      link={SData[3].links}/>
+
+<Card imgsrc={SData[4].imgsrc}
+      title={SData[4].title}
+      sname={SData[4].sname}
+      link={SData[4].links}/>
+  </>,
+  document.getElementById("root")
+)
+
